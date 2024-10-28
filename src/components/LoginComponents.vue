095bd3ce6fb07b3285243503ce3d9b41d675a00a
Wisnu-code <sugandawisnualkhusri@gmail.com>
2024-10-28 10:52:45 +0700

    Penginstalan fontawesome

--- src/components/LoginComponents.vue
+++ src/components/LoginComponents.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="flex justify-center items-center h-screen bg-sky-100">
         <!-- Left: Image -->
-        <div class="w-1/2 h-[80%] max-h-screen hidden lg:flex justify-center items-center bg-yellow-300 m-20 rounded-2xl">
+        <div
+            class="w-1/2 h-[80%] max-h-screen hidden lg:flex justify-center items-center bg-yellow-300 m-20 rounded-2xl">
             <img src="../assets/relax.png" alt="Placeholder Image" class="object-contain w-1/2 h-auto">
         </div>
         <!-- Right: Login Form -->
@@ -16,19 +17,28 @@
                         autocomplete="off">
                 </div>
                 <!-- Password Input -->
-                <div class="mb-4">
+                <div class="mb-4 relative">
                     <label for="password" class="block text-gray-600 font-semibold md:text-2xl text-lg">Password</label>
-                    <input type="password" v-model="password" id="password" name="password"
+                    <input :type="isPasswordVisible ? 'text' : 'password'" v-model="password" id="password"
+                        name="password"
                         class="block py-2.5 px-0 w-full text-lg md:text-2xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         autocomplete="off">
+                    <!-- Toggle Eye Icon -->
+                    <span @click="togglePasswordVisibility" class="absolute right-2 md:top-12 top-10 text-2xl cursor-pointer text-gray-500">
+                        <i :class="isPasswordVisible ? 'fa fa-eye' : 'fa fa-eye-slash'"></i>
+                    </span>
                 </div>
                 <!-- Login Button -->
-                <button type="submit" class="relative text-lg md:text-2xl md:font-bold font-semibold rounded-md h-[50px] w-full overflow-hidden border border-green-900 bg-white text-green-900 shadow-lg transition-all before:absolute before:left-0 before:right-0 before:top-0 before:h-0 before:w-full before:bg-green-900 before:duration-500 after:absolute after:bottom-0 after:left-0 after:right-0 after:h-0 after:w-full after:bg-green-900 after:duration-500 hover:text-white hover:shadow-green-900 hover:before:h-2/4 hover:after:h-2/4"><span class="relative z-10">Masuk</span></button>
+                <button type="submit"
+                    class="relative text-lg md:text-2xl md:font-bold font-semibold rounded-md h-[50px] w-full overflow-hidden border border-green-900 bg-white text-green-900 shadow-lg transition-all before:absolute before:left-0 before:right-0 before:top-0 before:h-0 before:w-full before:bg-green-900 before:duration-500 after:absolute after:bottom-0 after:left-0 after:right-0 after:h-0 after:w-full after:bg-green-900 after:duration-500 hover:text-white hover:shadow-green-900 hover:before:h-2/4 hover:after:h-2/4"><span
+                        class="relative z-10">Masuk</span></button>
             </form>
             <!-- Sign up Link -->
             <div class="mt-6 text-green-500 text-center text-lg md:text-2xl md:font-bold font-semibold">
                 <router-link to="/register">
-                    <button class="relative h-[50px] w-40 overflow-hidden text-green-400 transition-all before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:duration-500 after:absolute after:right-0 after:top-0 after:h-full after:w-0 after:duration-500 hover:text-white hover:shadow-green-400 hover:before:w-2/4 hover:before:bg-green-400 hover:after:w-2/4 hover:after:bg-green-400"><span class="relative z-10">Daftar Akun</span></button>
+                    <button
+                        class="relative h-[50px] w-40 overflow-hidden text-green-400 transition-all before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:duration-500 after:absolute after:right-0 after:top-0 after:h-full after:w-0 after:duration-500 hover:text-white hover:shadow-green-400 hover:before:w-2/4 hover:before:bg-green-400 hover:after:w-2/4 hover:after:bg-green-400 hover:scale-105 hover:shadow-lg"><span
+                            class="relative z-10">Daftar Akun</span></button>
 
                 </router-link>
             </div>
@@ -42,6 +52,7 @@ export default {
         return {
             username: '',
             password: '',
+            isPasswordVisible: false,
         };
     },
     methods: {
@@ -73,6 +84,9 @@ export default {
             } catch (error) {
                 alert('Error: ' + error.message);
             }
+        },
+        togglePasswordVisibility() {
+            this.isPasswordVisible = !this.isPasswordVisible;
         },
     },
 };
--- src/components/RegisterComponents.vue
+++ src/components/RegisterComponents.vue
@@ -19,13 +19,13 @@
                         autocomplete="off">
                 </div>
                 <!-- Password Input -->
-                <div class="mb-4">
+                <div class="mb-4 relative">
                     <label for="password" class="block text-lg font-semibold md:text-2xl text-gray-600">Password</label>
                     <input type="password" id="password" name="password"
                         class="block py-2.5 px-0 w-full text-lg font-semibold md:text-2xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         autocomplete="off">
                 </div>
-                <div class="mb-4">
+                <div class="mb-4 relative">
                     <label for="password" class="block text-lg font-semibold md:text-2xl text-gray-600">Ulangi Password</label>
                     <input type="password" id="password" name="password"
                         class="block py-2.5 px-0 w-full text-lg font-semibold md:text-2xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -43,7 +43,7 @@
             <!-- Untuk beralih kehalaman login -->
             <div class="mt-6 text-green-500 text-center text-lg md:text-2xl md:font-bold font-semibold">
                 <router-link to="/login">
-                    <button class="relative h-[50px] w-40 overflow-hidden text-green-400 transition-all before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:duration-500 after:absolute after:right-0 after:top-0 after:h-full after:w-0 after:duration-500 hover:text-white hover:shadow-green-400 hover:before:w-2/4 hover:before:bg-green-400 hover:after:w-2/4 hover:after:bg-green-400"><span class="relative z-10">Masuk Di Sini</span></button>
+                    <button class="relative h-[50px] w-40 overflow-hidden text-green-400 transition-all before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:duration-500 after:absolute after:right-0 after:top-0 after:h-full after:w-0 after:duration-500 hover:text-white hover:shadow-green-400 hover:before:w-2/4 hover:before:bg-green-400 hover:after:w-2/4 hover:after:bg-green-400 hover:scale-105 hover:shadow-lg"><span class="relative z-10">Masuk Di Sini</span></button>
                 </router-link>
             </div>
         </div>
@@ -55,8 +55,11 @@
     </div>
 </template>
 
-<script>
+<script setup>
 import { useRouter } from 'vue-router';
+</script>
+
+<script>
 const router = useRouter()
     document.addEventListener('DOMContentLoaded', function() {
         const form = document.querySelector('form');
