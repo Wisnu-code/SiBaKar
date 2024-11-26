b871cd5d06b5d6b4382319ee9d2d5042e4b8b0e6
Wisnu-code <sugandawisnualkhusri@gmail.com>
2024-11-26 10:51:57 +0700

    Penambahan tombol delete pada dashboard event

--- src/components/DashboardEventComponents.vue
+++ src/components/DashboardEventComponents.vue
@@ -42,6 +42,9 @@
                                                 <th scope="col"
                                                     class="px-6 py-3 text-start font-medium text-gray-500 uppercase">
                                                     Details</th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 text-start font-medium text-gray-500 uppercase">
+                                                    Action</th>
                                             </tr>
                                         </thead>
                                         <tbody class="divide-y divide-gray-200">
@@ -61,6 +64,9 @@
                                                 <td
                                                     class="px-6 py-4 whitespace-nowrap text-sm md:text-lg text-gray-800">
                                                     {{ event.detail }}
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm md:text-lg text-gray-800">
+                                                    <button type="button" class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">Delete</button>
                                                 </td>
                                             </tr>
                                         </tbody>
